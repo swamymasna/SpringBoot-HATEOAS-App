49c0a8d21353f32957b5e93b2a938f0732a04bdf
swamymasna <swamymasna55@gmail.com>
2022-09-13 17:59:01 +0530

    added method

--- src/main/java/com/swamy/model/Product.java
+++ src/main/java/com/swamy/model/Product.java
@@ -9,10 +9,10 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Product extends RepresentationModel<Product>{
+public class Product extends RepresentationModel<Product> {
+// Use RepresentationModel<Product> in Binding class only.
 
 	private Integer pid;
 	private String pname;
 	private Double pcost;
 }
-
--- src/main/java/com/swamy/rest/ProductRestController.java
+++ src/main/java/com/swamy/rest/ProductRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,21 +20,32 @@ public class ProductRestController {
 
 	@GetMapping("/one/{pid}")
 	public Product getOneProduct(@PathVariable Integer pid) {
-		
+
 		Product p = new Product(99, "TV", 58000.00);
 
-		Link link = WebMvcLinkBuilder.linkTo(
-				WebMvcLinkBuilder.methodOn(ProductRestController.class)
-				.getAllProducts()).withSelfRel();
+		Link link = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(ProductRestController.class).getAllProducts())
+				.withSelfRel();
 		p.add(link);
 		return p;
 	}
-	
+
 	@GetMapping("/list")
 	public List<Product> getAllProducts() {
 		List<Product> list = new ArrayList<>();
 		list.add(new Product(100, "AC", 96000.00));
 		list.add(new Product(101, "COOLER", 18000.00));
 		return list;
+	}
+
+	// ------Using ResponseEntity<T>--------------------------------------//
+	@GetMapping("/pob/{pid}")
+	public ResponseEntity<Product> getOneProductById(@PathVariable Integer pid) {
+
+		Product product = new Product(99, "TV", 58000.00);
+
+		Link link = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(ProductRestController.class).getAllProducts())
+				.withSelfRel();
+		product.add(link);
+		return new ResponseEntity<Product>(product, HttpStatus.OK);
 	}
 }
